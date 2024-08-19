1504b29b8df9e91b6a01c455a8335b256a842ded
williamssuarez <wikleson7@gmail.com>
2024-08-19 18:32:39 -0400

    [Database] Estructura

--- asistco.sql
+++ asistco.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-08-2024 a las 20:17:47
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
+-- Tiempo de generación: 20-08-2024 a las 00:31:12
+-- Versión del servidor: 10.4.25-MariaDB
+-- Versión de PHP: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,13 +27,15 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `asistencia`
 --
 
-CREATE TABLE `asistencia` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `asistencia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `empleado_id` int(11) NOT NULL,
   `hora` time NOT NULL,
   `fecha` date NOT NULL,
-  `tipo` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
+  `tipo` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `fk_empleado_id` (`empleado_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- --------------------------------------------------------
 
@@ -41,12 +43,29 @@ CREATE TABLE `asistencia` (
 -- Estructura de tabla para la tabla `compensatorios`
 --
 
-CREATE TABLE `compensatorios` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `compensatorios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
-  `compensatorios_restantes` int(11) NOT NULL,
-  `compensatorios_totales` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `fecha` date NOT NULL,
+  `estado` tinyint(1) NOT NULL DEFAULT 0,
+  `observaciones` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `compensatorios_usuarios`
+--
+
+CREATE TABLE IF NOT EXISTS `compensatorios_usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `compensatorios_totales` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -54,23 +73,15 @@ CREATE TABLE `compensatorios` (
 -- Estructura de tabla para la tabla `empleados`
 --
 
-CREATE TABLE `empleados` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `empleados` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `documento_numero` varchar(100) NOT NULL,
   `telefono` varchar(50) NOT NULL,
-  `codigo` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `empleados`
---
-
-INSERT INTO `empleados` (`id`, `nombre`, `apellido`, `documento_numero`, `telefono`, `codigo`) VALUES
-(1, 'emp1', 'emp1', '1111111', '242424', ''),
-(2, 'emp1', 'emp1', '1111111', '242424', '123'),
-(5, 'emp1', 'emp1', '1111111', '242424', '123');
+  `codigo` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- --------------------------------------------------------
 
@@ -78,23 +89,17 @@ INSERT INTO `empleados` (`id`, `nombre`, `apellido`, `documento_numero`, `telefo
 -- Estructura de tabla para la tabla `guardia`
 --
 
-CREATE TABLE `guardia` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `guardia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `fecha_inicio` datetime NOT NULL DEFAULT current_timestamp(),
   `fecha_fin` datetime NOT NULL DEFAULT current_timestamp(),
   `user_id` int(11) NOT NULL,
   `observaciones` varchar(200) DEFAULT NULL,
-  `estado` tinyint(4) DEFAULT 1,
-  `isDone` tinyint(1) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=DYNAMIC;
-
---
--- Volcado de datos para la tabla `guardia`
---
-
-INSERT INTO `guardia` (`id`, `fecha_inicio`, `fecha_fin`, `user_id`, `observaciones`, `estado`, `isDone`) VALUES
-(13, '2024-08-12 06:59:00', '2024-08-17 18:59:00', 1, 'asdasda', 2, 0),
-(15, '2024-08-05 08:00:00', '2024-08-09 08:59:00', 6, 'Guardia Urgente', 1, 1);
+  `estado` tinyint(4) NOT NULL DEFAULT 1,
+  `isDone` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `user_id` (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 -- --------------------------------------------------------
 
@@ -102,96 +107,17 @@ INSERT INTO `guardia` (`id`, `fecha_inicio`, `fecha_fin`, `user_id`, `observacio
 -- Estructura de tabla para la tabla `usuarios`
 --
 
-CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(50) NOT NULL,
   `apellido` varchar(50) NOT NULL,
   `login` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `imagen` varchar(100) NOT NULL,
-  `estado` tinyint(4) NOT NULL DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `usuarios`
---
-
-INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `login`, `email`, `password`, `imagen`, `estado`) VALUES
-(1, 'admin', 'admin', 'admin', 'Admin@pdvsa.com', '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2', '1721660953.png', 1),
-(5, 'admin12', 'admin1', 'admin1', 'admin1@pdvsa.com', 'root', '', 1),
-(6, 'Jose', 'Perez', 'jose', 'Jose@gmail.com', 'a5ed602ee512bda8b2b18d6d4b06d6f176e7e3fb15a0cf5b23028b9849bd0d62', '', 1),
-(7, 'Williams', 'Suarez', 'williamssuarez', 'wikleson7@gmail.com', 'f66baadd0f8189580001719b72374a6f08c1ef5ef5ecb88623bb7039225f992e', '1723932265.jpg', 1);
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `asistencia`
---
-ALTER TABLE `asistencia`
-  ADD PRIMARY KEY (`id`) USING BTREE,
-  ADD KEY `fk_empleado_id` (`empleado_id`) USING BTREE;
-
---
--- Indices de la tabla `compensatorios`
---
-ALTER TABLE `compensatorios`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `guardia`
---
-ALTER TABLE `guardia`
-  ADD PRIMARY KEY (`id`) USING BTREE,
-  ADD KEY `user_id` (`user_id`) USING BTREE;
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `asistencia`
---
-ALTER TABLE `asistencia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `compensatorios`
---
-ALTER TABLE `compensatorios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `guardia`
---
-ALTER TABLE `guardia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  `estado` tinyint(4) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 --
 -- Restricciones para tablas volcadas
@@ -202,6 +128,18 @@ ALTER TABLE `usuarios`
 --
 ALTER TABLE `asistencia`
   ADD CONSTRAINT `asistencia_ibfk_1` FOREIGN KEY (`empleado_id`) REFERENCES `empleados` (`id`);
+
+--
+-- Filtros para la tabla `compensatorios`
+--
+ALTER TABLE `compensatorios`
+  ADD CONSTRAINT `compensatorios_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `compensatorios_usuarios`
+--
+ALTER TABLE `compensatorios_usuarios`
+  ADD CONSTRAINT `compensatorios_usuarios_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usuarios` (`id`);
 
 --
 -- Filtros para la tabla `guardia`
